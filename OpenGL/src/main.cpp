a568beee1ccae0bbb49f2f83d098fa430c2d41b0
Kourosh Riahidehkordi <Kourosh.Riahidehkordi@iit.it>
2023-11-23 17:16:00 +0100

    Project 1 - Hello World

--- OpenGL/src/main.cpp
+++ OpenGL/src/main.cpp
@@ -1,30 +1,5 @@
-/// GLEW is needed to provide OpenGL extensions.
-//#define GLEW_STATIC
-#include <GL/glew.h>
-
 #include <SFML/Window.hpp>
 #include <SFML/OpenGL.hpp>
-
-#include <iostream>
-#include <vector>
-
-
-
-// Vertex Shader source code
-const char* vertexShaderSource = "#version 330 core\n"
-"layout (location = 0) in vec3 aPos;\n"
-"void main()\n"
-"{\n"
-"   gl_Position = vec4(aPos.x, aPos.y, aPos.z, 1.0);\n"
-"}\0";
-//Fragment Shader source code
-const char* fragmentShaderSource = "#version 330 core\n"
-"out vec4 FragColor;\n"
-"void main()\n"
-"{\n"
-"   FragColor = vec4(0.8f, 0.3f, 0.02f, 1.0f);\n"
-"}\n\0";
-
 
 
 int main()
@@ -36,88 +11,17 @@ int main()
     // activate the window
     window.setActive(true);
 
-    // load resources, initialize the OpenGL states, ... ----------------------------------------------------------------
-
-
-
-	//  initialize GLEW 
-	GLenum err = glewInit();
-	// Create Vertex Shader Object and get its reference
-	GLuint vertexShader = glCreateShader(GL_VERTEX_SHADER);
-	// Attach Vertex Shader source to the Vertex Shader Object
-	glShaderSource(vertexShader, 1, &vertexShaderSource, NULL);
-	// Compile the Vertex Shader into machine code
-	glCompileShader(vertexShader);
-
-	// Create Fragment Shader Object and get its reference
-	GLuint fragmentShader = glCreateShader(GL_FRAGMENT_SHADER);
-	// Attach Fragment Shader source to the Fragment Shader Object
-	glShaderSource(fragmentShader, 1, &fragmentShaderSource, NULL);
-	// Compile the Vertex Shader into machine code
-	glCompileShader(fragmentShader);
-
-	// Create Shader Program Object and get its reference
-	GLuint shaderProgram = glCreateProgram();
-	// Attach the Vertex and Fragment Shaders to the Shader Program
-	glAttachShader(shaderProgram, vertexShader);
-	glAttachShader(shaderProgram, fragmentShader);
-	// Wrap-up/Link all the shaders together into the Shader Program
-	glLinkProgram(shaderProgram);
-
-	// Delete the now useless Vertex and Fragment Shader objects
-	glDeleteShader(vertexShader);
-	glDeleteShader(fragmentShader);
-
-
-
-	// Vertices coordinates
-	GLfloat vertices[] =
-	{
-		-0.5f, -0.5f * float(sqrt(3)) / 3, 0.0f, // Lower left corner
-		0.5f, -0.5f * float(sqrt(3)) / 3, 0.0f, // Lower right corner
-		0.0f, 0.5f * float(sqrt(3)) * 2 / 3, 0.0f // Upper corner
-	};
-
-	// Create reference containers for the Vartex Array Object and the Vertex Buffer Object
-	GLuint VAO, VBO;
-
-	// Generate the VAO and VBO with only 1 object each
-	glGenVertexArrays(1, &VAO);
-	glGenBuffers(1, &VBO);
-
-	// Make the VAO the current Vertex Array Object by binding it
-	glBindVertexArray(VAO);
-
-	// Bind the VBO specifying it's a GL_ARRAY_BUFFER
-	glBindBuffer(GL_ARRAY_BUFFER, VBO);
-	// Introduce the vertices into the VBO
-	glBufferData(GL_ARRAY_BUFFER, sizeof(vertices), vertices, GL_STATIC_DRAW);
-
-	// Configure the Vertex Attribute so that OpenGL knows how to read the VBO
-	glVertexAttribPointer(0, 3, GL_FLOAT, GL_FALSE, 3 * sizeof(float), (void*)0);
-	// Enable the Vertex Attribute so that OpenGL knows to use it
-	glEnableVertexAttribArray(0);
-
-	// Bind both the VBO and VAO to 0 so that we don't accidentally modify the VAO and VBO we created
-	glBindBuffer(GL_ARRAY_BUFFER, 0);
-	glBindVertexArray(0);
+    // load resources, initialize the OpenGL states, ... =======================================================================
 
 
 
 
-
-
-
-
-
-
-
-
-
-
-
-    // --------------------------------------------------------------------------------------------------------------------
+    // -------------------------------------------------------------------------------------------------------------------------
     
+    // Animation variables
+    float time = 0.0f;
+    const float animationSpeed = 5.0f; // Speed of color change
+
     // run the main loop
     bool running = true;
     while (running)
@@ -126,7 +30,7 @@ int main()
         sf::Event event;
         while (window.pollEvent(event))
         {
-            if (event.type == sf::Event::Closed)
+            if (event.type == sf::Event::Closed || event.key.code == sf::Keyboard::Escape)
             {
                 // end the program
                 running = false;
@@ -138,43 +42,34 @@ int main()
             }
         }
 
+        // Increase time for smooth color change
+        time += 0.01f; 
+
         // clear the buffers
         glClear(GL_COLOR_BUFFER_BIT | GL_DEPTH_BUFFER_BIT);
 
-        // draw... --------------------------------------------------------------------------------------------
+        // draw... ==============================================================================================================
 
-
-		// Specify the color of the background
-		glClearColor(0.07f, 0.13f, 0.17f, 1.0f);
-		// Clean the back buffer and assign the new color to it
-		glClear(GL_COLOR_BUFFER_BIT);
-		// Tell OpenGL which Shader Program we want to use
-		glUseProgram(shaderProgram);
-		// Bind the VAO so OpenGL knows to use it
-		glBindVertexArray(VAO);
-		// Draw the triangle using the GL_TRIANGLES primitive
-		glDrawArrays(GL_TRIANGLES, 0, 3);
+        glClearColor(std::sin(time * animationSpeed), 0.0, 0, 0);
 
 
 
 
 
-        // --------------------------------------------------------------------------------------------
+
+        // ----------------------------------------------------------------------------------------------------------------------
 
         // end the current frame (internally swaps the front and back buffers)
         window.display();
     }
 
-    // release resources... ---------------------------------------------------------------------------------------------
-
-	// Delete all the objects we've created
-	glDeleteVertexArrays(1, &VAO);
-	glDeleteBuffers(1, &VBO);
-	glDeleteProgram(shaderProgram);
+    // release resources... =====================================================================================================
 
 
 
-    // --------------------------------------------------------------------------------------------
+
+
+    // --------------------------------------------------------------------------------------------------------------------------
 
     return 0;
 }
