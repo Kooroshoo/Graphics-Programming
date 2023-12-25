e26196bbb39021de5dff2f594d630bb133eaa0b0
Kourosh <kooroshoo@yahoo.com>
2023-12-25 18:37:18 +0100

    Project 2 - Transformations

--- OpenGL/src/main.cpp
+++ OpenGL/src/main.cpp
@@ -1,123 +1,210 @@
+// Defined before OpenGL and GLUT includes to avoid deprecation message in OSX
+#define GL_SILENCE_DEPRECATION
+#include <iostream>
 #include <GL/glew.h>
 #include <GL/glfw3.h>
-
-#include <iostream>
-
+#include "cyCodeBase/cyTriMesh.h"
+#include "cyCodeBase/cyMatrix.h"
 #include "cyCodeBase/cyGL.h"
 
-#include "shaderClass.h"
-#include "VAO.h"
-#include "VBO.h"
-#include "EBO.h"
+int width = 1280, height = 720;
+GLFWwindow* window;
+cy::TriMesh teapotMesh;
+cy::GLSLProgram prog;
+bool doRotate = false, doZoom = false;
+double rotX = 5.23599, rotY = 0, distZ = 40;
+double lastX, lastY;
 
-// Vertices coordinates
-GLfloat vertices[] =
+GLclampf Red = 0.0f, Green = 0.0f, Blue = 0.0f, Alpha = 1.0f;
+
+void renderScene()
 {
-	-0.5f, -0.5f * float(sqrt(3)) / 3, 0.0f, // Lower left corner
-	0.5f, -0.5f * float(sqrt(3)) / 3, 0.0f, // Lower right corner
-	0.0f, 0.5f * float(sqrt(3)) * 2 / 3, 0.0f, // Upper corner
-	-0.5f / 2, 0.5f * float(sqrt(3)) / 6, 0.0f, // Inner left
-	0.5f / 2, 0.5f * float(sqrt(3)) / 6, 0.0f, // Inner right
-	0.0f, -0.5f * float(sqrt(3)) / 3, 0.0f // Inner down
-};
+    // Clear Color buffer
+    glClear(GL_COLOR_BUFFER_BIT | GL_DEPTH_BUFFER_BIT);
+    glClearColor(Red, Green, Blue, Alpha);
+    glDrawArrays(GL_POINTS, 0, teapotMesh.NV());
+}
 
-// Indices for vertices order
-GLuint indices[] =
+bool compileShaders() {
+    bool shaderSuccess = prog.BuildFiles("shaders/shader.vert", "shaders/shader.frag");
+    return shaderSuccess;
+}
+
+void processNormalKeyCB(GLFWwindow* window, int key, int scancode, int action, int mods) {
+    if (key == GLFW_KEY_ESCAPE && action == GLFW_PRESS)
+        glfwSetWindowShouldClose(window, GLFW_TRUE);
+    else if (key == GLFW_KEY_F6 && action == GLFW_PRESS) {
+        std::cout << "Recompiling shaders..." << std::endl;
+        bool shaderSuccess = compileShaders();
+        if (!shaderSuccess) {
+            std::cout << "Error Recompiling shaders" << std::endl;
+        }
+    }
+}
+
+static void error_callback(int error, const char* description)
 {
-	0, 3, 5, // Lower left triangle
-	3, 2, 4, // Lower right triangle
-	5, 4, 1 // Upper triangle
-};
+    std::cerr << "Error: " << description << std::endl;
+}
+double deg2rad(double degrees) {
+    return degrees * 4.0 * atan(1.0) / 180.0;
+}
 
-
-
-int main()
+void processMouseButtonCB(GLFWwindow* window, int button, int action, int mods)
 {
-	// Initialize GLFW
-	glfwInit();
+    double xpos, ypos;
+    if (button == GLFW_MOUSE_BUTTON_RIGHT && action == GLFW_PRESS) {
+        doZoom = true;
+        glfwGetCursorPos(window, &xpos, &ypos);
+        lastX = xpos;
+        lastY = ypos;
+    }
+    else if (button == GLFW_MOUSE_BUTTON_RIGHT && action == GLFW_RELEASE) {
+        doZoom = false;
+        glfwGetCursorPos(window, &xpos, &ypos);
+        lastX = xpos;
+        lastY = ypos;
+    }
+    else if (button == GLFW_MOUSE_BUTTON_LEFT && action == GLFW_PRESS) {
+        doRotate = true;
+        glfwGetCursorPos(window, &xpos, &ypos);
+        lastX = xpos;
+        lastY = ypos;
+    }
+    else if (button == GLFW_MOUSE_BUTTON_LEFT && action == GLFW_RELEASE) {
+        doRotate = false;
+        glfwGetCursorPos(window, &xpos, &ypos);
+        lastX = xpos;
+        lastY = ypos;
+    }
+}
+
+void processMousePosCB(GLFWwindow* window, double xpos, double ypos)
+{
+    double xDiff = lastX - xpos;
+    double yDiff = lastY - ypos;
+    // Calculate camera zoom based on mouse movement in y direction
+    if (doZoom) {
+        distZ += yDiff * 0.05;
+    }
+
+    // Calculate camera rotation based on mouse movement in x direction
+    if (doRotate) {
+        rotX -= yDiff * 0.005;
+        rotY -= xDiff * 0.005;
+    }
+
+    lastX = xpos;
+    lastY = ypos;
+}
+
+int main(int argc, char** argv)
+{
+    glfwSetErrorCallback(error_callback);
+
+    // Initialize GLFW
+    if (!glfwInit())
+        exit(EXIT_FAILURE);
+
+    // Create a windowed mode window and its OpenGL context
+    glfwWindowHint(GLFW_CONTEXT_VERSION_MAJOR, 3);
+    glfwWindowHint(GLFW_CONTEXT_VERSION_MINOR, 3);
+    glfwWindowHint(GLFW_OPENGL_FORWARD_COMPAT, GL_TRUE);
+    glfwWindowHint(GLFW_OPENGL_PROFILE, GLFW_OPENGL_CORE_PROFILE);
+    window = glfwCreateWindow(width, height, "Transformations", NULL, NULL);
+    if (!window)
+    {
+        glfwTerminate();
+        exit(EXIT_FAILURE);
+    }
+    glfwMakeContextCurrent(window);
+
+    //Initialize GLEW
+    glewExperimental = GL_TRUE;
+    GLenum err = glewInit();
+    if (GLEW_OK != err)
+    {
+        std::cout << "Error: " << glewGetErrorString(err) << std::endl;
+        return 1;
+    }
+
+    // Print and test OpenGL context infos
+    std::cout << glGetString(GL_VERSION) << std::endl;
+    std::cout << glGetString(GL_RENDERER) << std::endl;
+
+    // Setup GLFW callbacks
+    glfwSetKeyCallback(window, processNormalKeyCB);
+    glfwSetMouseButtonCallback(window, processMouseButtonCB);
+    glfwSetCursorPosCallback(window, processMousePosCB);
+    glfwSwapInterval(1);
+
+    //OpenGL initializations
+    glViewport(0, 0, width, height);
+    // CY_GL_REGISTER_DEBUG_CALLBACK;
+
+    //Load object mesh
+    bool meshSuccess = teapotMesh.LoadFromFileObj("objects/teapot.obj");
+    if (!meshSuccess)
+    {
+        std::cout << "Error loading mesh" << std::endl;
+        return 1;
+    }
+
+    //Compute bounding box
+    teapotMesh.ComputeBoundingBox();
+    cy::Vec3f teapotCenter = (teapotMesh.GetBoundMax() - teapotMesh.GetBoundMin()) / 2;
+    cy::Vec3f teapotOrigin = teapotMesh.GetBoundMin() + teapotCenter;
 
 
+    //Create vertex array object
+    GLuint vao;
+    glGenVertexArrays(1, &vao);
+    glBindVertexArray(vao);
 
-	// Tell GLFW what version of OpenGL we are using 
-	// In this case we are using OpenGL 3.3
-	glfwWindowHint(GLFW_CONTEXT_VERSION_MAJOR, 3);
-	glfwWindowHint(GLFW_CONTEXT_VERSION_MINOR, 3);
-	// Tell GLFW we are using the CORE profile
-	// So that means we only have the modern functions
-	glfwWindowHint(GLFW_OPENGL_PROFILE, GLFW_OPENGL_CORE_PROFILE);
+    //Create vertex buffer object
+    GLuint vbo;
+    glGenBuffers(1, &vbo);
+    glBindBuffer(GL_ARRAY_BUFFER, vbo);
+    glBufferData(GL_ARRAY_BUFFER, sizeof(cy::Vec3f) * teapotMesh.NV(), &teapotMesh.V(0), GL_STATIC_DRAW);
 
-	// Create a GLFWwindow object of 800 by 800 pixels, naming it "YoutubeOpenGL"
-	GLFWwindow* window = glfwCreateWindow(800, 800, "YoutubeOpenGL", NULL, NULL);
-	// Error check if the window fails to create
-	if (window == NULL)
-	{
-		std::cout << "Failed to create GLFW window" << std::endl;
-		glfwTerminate();
-		return -1;
-	}
-	// Introduce the window into the current context
-	glfwMakeContextCurrent(window);
+    //Setup Shader program
+    std::cout << "Compiling shaders..." << std::endl;
+    bool shaderSuccess = compileShaders();
+    if (!shaderSuccess)
+    {
+        std::cout << "Error compiling shaders" << std::endl;
+        return 1;
+    }
 
-	//Load GLEW so it configures OpenGL
-	glewInit();
-	// Specify the viewport of OpenGL in the Window
-	// In this case the viewport goes from x = 0, y = 0, to x = 800, y = 800
-	glViewport(0, 0, 800, 800);
+    //Loop until the user closes the window
+    while (!glfwWindowShouldClose(window))
+    {
+        // Calculate camera transformations
+        cy::Matrix3f rotXMatrix = cy::Matrix3f::RotationX(rotX);
+        cy::Matrix3f rotYMatrix = cy::Matrix3f::RotationY(rotY);
+        cy::Matrix4f transMatrix = cy::Matrix4f::Translation(-teapotOrigin);
+        cy::Matrix4f view = cy::Matrix4f::View(cy::Vec3f(0, 0, distZ), cy::Vec3f(0, 0, 0), cy::Vec3f(0, 1, 0));
+        cy::Matrix4f projMatrix = cy::Matrix4f::Perspective(deg2rad(60), float(width) / float(height), 0.1f, 1000.0f);
+        cy::Matrix4f mvp = projMatrix * view * rotXMatrix * rotYMatrix * transMatrix;
 
+        //Set Program and Program Attributes
+        prog.Bind();
+        prog["mvp"] = mvp;
+        GLuint pos_location = glGetAttribLocation(prog.GetID(), "pos");
+        glEnableVertexAttribArray(pos_location);
+        glVertexAttribPointer(pos_location, 3, GL_FLOAT, GL_FALSE, 0, (GLvoid*)0);
 
+        renderScene();
 
-	// Generates Shader object using shaders defualt.vert and default.frag
-	Shader shaderProgram("src/default.vert", "src/default.frag");
+        // Swap front and back buffers
+        glfwSwapBuffers(window);
 
+        // Poll for and process events
+        glfwPollEvents();
+    }
 
+    glfwDestroyWindow(window);
+    glfwTerminate();
 
-	// Generates Vertex Array Object and binds it
-	VAO VAO1;
-	VAO1.Bind();
-
-	// Generates Vertex Buffer Object and links it to vertices
-	VBO VBO1(vertices, sizeof(vertices));
-	// Generates Element Buffer Object and links it to indices
-	EBO EBO1(indices, sizeof(indices));
-
-	// Links VBO to VAO
-	VAO1.LinkVBO(VBO1, 0);
-	// Unbind all to prevent accidentally modifying them
-	VAO1.Unbind();
-	VBO1.Unbind();
-	EBO1.Unbind();
-
-
-
-	// Main while loop
-	while (!glfwWindowShouldClose(window))
-	{
-		// Specify the color of the background
-		glClearColor(0.07f, 0.13f, 0.17f, 1.0f);
-		// Clean the back buffer and assign the new color to it
-		glClear(GL_COLOR_BUFFER_BIT);
-		// Tell OpenGL which Shader Program we want to use
-		shaderProgram.Activate();
-		// Bind the VAO so OpenGL knows to use it
-		VAO1.Bind();
-		// Draw primitives, number of indices, datatype of indices, index of indices
-		glDrawElements(GL_TRIANGLES, 9, GL_UNSIGNED_INT, 0);
-		// Swap the back buffer with the front buffer
-		glfwSwapBuffers(window);
-		// Take care of all GLFW events
-		glfwPollEvents();
-	}
-
-
-
-	// Delete all the objects we've created
-	VAO1.Delete();
-	VBO1.Delete();
-	EBO1.Delete();
-	shaderProgram.Delete();
-	// Delete window before ending the program
-	glfwDestroyWindow(window);
-	// Terminate GLFW before ending the program
-	glfwTerminate();
-	return 0;
+    return 0;
 }
